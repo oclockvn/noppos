f91f0a0b72f7cf06e27f2d2e480d85cefe59d253
oclockvn <oclockvn@gmail.com>
2018-07-12 22:32:49 +0700

    added action bar

--- theme/js/app.js
+++ theme/js/app.js
@@ -1,12 +1,33 @@
+class Product {
+    constructor() {
+        this.isChecked = ko.observable(false);
+    }
+}
+
 class AppViewModel {
     constructor() {
-        this.cartItems = ko.observableArray([1,2,3,4,5,6]);
+        this.cartItems = ko.observableArray([]);
         this.categories = ko.observableArray([1,2,3,4,5,6]);
         this.products = ko.observableArray([]);
+
+        this.hasSelectedItems = ko.computed(() => this.cartItems().some(x => x.isChecked()), this);
+        this.checkedItems = ko.computed(() => this.cartItems().filter(x => x.isChecked()).length, this);
 
         for (let index = 0; index < 30; index++) {
             this.products.push(index);
         }
+
+        for (let index = 0; index < 10; index++) {
+            this.cartItems.push(new Product());
+        }
+    }
+    
+    toggle(data) {
+        data.isChecked(!data.isChecked());
+    }
+
+    uncheckAll() {
+        this.cartItems().forEach(x => x.isChecked(false));
     }
 }
 
